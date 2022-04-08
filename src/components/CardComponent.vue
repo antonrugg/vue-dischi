8d2f6a8962fd88127db24e8c5ba47780be14213a
Antonio Ruggiero <antonrugg@gmail.com>
2022-04-08 18:08:43 +0200

    styling card

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="card">
-      Card component
-  </div>
+    <main class="col-12 col-md-3 col-sm-4">
+        <div class="card d-flex flex-column">
+            <img src="https://picsum.photos/300/200" alt="card image test">
+            <p class="title">title</p>
+            <p class="sub-title">sub-title</p>
+        </div>
+    </main>
 </template>
 
 <script>
@@ -11,5 +15,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    @import '@/style/variables';
+
+    .card{
+        background-color: $bg-color-nav-and-card;
+        .title{
+            color: white;
+        }
+
+        .sub-title{
+            color: $color-card-sub-text;
+        }
+    }
 
 </style>
